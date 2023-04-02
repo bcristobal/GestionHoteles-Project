b474918ad3a15ae0276bc04d2314ca8f7f45ee81
Beñat Cristobal <b.cristobal@opendeusto.es>
2023-04-02 20:10:21 +0200

    Crear carpepa bd

---
+++ GestionHoteles/BD/base_datos.c
@@ -0,0 +1,8 @@
+/*
+ * base_datos.c
+ *
+ *  Created on: 2 abr 2023
+ *      Author: benat
+ */
+
+#include "base_datos.h"
---
+++ GestionHoteles/BD/base_datos.h
@@ -0,0 +1,13 @@
+/*
+ * base_datos.h
+ *
+ *  Created on: 2 abr 2023
+ *      Author: benat
+ */
+
+#ifndef BASE_DATOS_H_
+#define BASE_DATOS_H_
+
+
+
+#endif /* BASE_DATOS_H_ */
