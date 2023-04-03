5859982c4b1b348dca14d31c1f356a6cc816de9f
Unai Alonso <unai.alonso@opendeusto.es>
2023-04-03 11:44:39 +0200

    Logger creado

---
+++ GestionHoteles/logger/log.c
@@ -0,0 +1,22 @@
+/*
+ * log.c
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: unaialonso
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+void logMensaje(char* mensaje) {
+   time_t t = time(NULL);
+   struct tm *tm = localtime(&t);
+   char file[] = "logger.txt";
+   FILE* fp = fopen(file, "a");
+   if(fp != NULL) {
+      fprintf(fp, "%04d-%02d-%02d %02d:%02d:%02d - %s\n",
+         tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday,
+         tm->tm_hour, tm->tm_min, tm->tm_sec, mensaje);
+      fclose(fp);
+   }
+}
---
+++ GestionHoteles/logger/log.h
@@ -0,0 +1,13 @@
+/*
+ * log.h
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: unaialonso
+ */
+
+#ifndef LOGGER_LOG_H_
+#define LOGGER_LOG_H_
+
+void logMensaje(char* mensaje);
+
+#endif /* LOGGER_LOG_H_ */
