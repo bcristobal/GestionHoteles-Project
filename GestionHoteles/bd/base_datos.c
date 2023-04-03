8e73d3a18aac0a4c188dec9bb55702c93e44ad2e
Beñat Cristobal <b.cristobal@opendeusto.es>
2023-04-03 17:12:44 +0200

    Corregir validarAdmin

--- GestionHoteles/bd/base_datos.c
+++ GestionHoteles/bd/base_datos.c
@@ -5,34 +5,40 @@ int validadAdmin(char* usuario, char* clave, sqlite3 *db) {
 	char c[30];
 
 	sqlite3_stmt *stmt;
-	char sql[] = "SELECT CLAVE FROM ADMINISTRADOR WHERE USUARIO = ?";
+	char sql[] = "SELECT CLAVE_ADMIN FROM ADMINISTRADOR WHERE USUARIO_ADMIN = ?";
 
 	int result = sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL) ;
 	if (result != SQLITE_OK) {
 		printf("Error preparing statement (SELECT)\n");
 		printf("%s\n", sqlite3_errmsg(db));
 	}
-	printf("SQL query prepared (SELECT)\n");
+	else {
+		printf("SQL query prepared (SELECT)\n");
+	}
 
 	result = sqlite3_bind_text(stmt, 1, usuario, strlen(usuario), SQLITE_STATIC);
 	if (result != SQLITE_OK) {
 		printf("Error binding parameters\n");
 		printf("%s\n", sqlite3_errmsg(db));
-		return result;
+	}
+	else {
+		printf("Parameters binded\n");
 	}
 
 	result = sqlite3_step(stmt) ;
 	if (result == SQLITE_ROW) {
-		strcpy(c, (char *) sqlite3_column_text(stmt, 1));
+		strcpy(c, (char *) sqlite3_column_text(stmt, 0));
+		printf("se copia en c\n");
 	}
 
 	result = sqlite3_finalize(stmt);
 		if (result != SQLITE_OK) {
 			printf("Error finalizing statement (SELECT)\n");
 			printf("%s\n", sqlite3_errmsg(db));
-			return result;
 		}
-		printf("Prepared statement finalized (SELECT)\n");
+		else {
+			printf("Prepared statement finalized (SELECT)\n");
+		}
 
 	return strcmp(c, clave);
 }
