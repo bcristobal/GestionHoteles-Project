21f6fd01444a7032116b1c33a39d804a8ab08bba
Unai Alonso <unai.alonso@opendeusto.es>
2023-04-03 18:13:04 +0200

    Funcion de guardar reserva

---
+++ GestionHoteles/ficheros/ficheros.h
@@ -0,0 +1,13 @@
+/*
+ * ficheros.h
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: unaialonso
+ */
+
+#ifndef FICHEROS_FICHEROS_H_
+#define FICHEROS_FICHEROS_H_
+
+void guardarReserva(char cod_Reserva[], char fa_ini[], char fa_fin[], char id_hotel[], int num_Personas, char importe[]);
+
+#endif /* FICHEROS_FICHEROS_H_ */
